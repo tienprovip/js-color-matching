fc13b3d2f940b07b8be6623408efaa796e293b7c
Ptien1305 <ptien1305@gmail.com>
2022-08-23 11:14:58 +0700

    integrate game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
-import { getColorElementList, getColorListElement } from './selectors.js';
+import { getColorElementList, getColorListElement, getInActiveColorList } from './selectors.js';
 import { getRandomColorPairs } from './utils.js'
 
 // Global variables
 let selections = []
-let gameState = GAME_STATUS.PLAYING
+let gameStatus = GAME_STATUS.PLAYING
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -14,7 +14,50 @@ let gameState = GAME_STATUS.PLAYING
 // 5. Handle replay click
 
 function handleColorClick(liElement) {
+    const shouldBlockClick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameStatus);
+    if (!liElement || shouldBlockClick) return;
+
+    // show color for clicked cell
     liElement.classList.add('active');
+
+    // save clicked cell to selections
+    selections.push(liElement);
+    if (selections.length < 2) return;
+    // check match
+    const firstColor = selections[0].dataset.color;
+    const secondColor = selections[1].dataset.color;
+    const isMatch = firstColor === secondColor;
+    if (isMatch) {
+        // check win
+        const isWin = getInActiveColorList().length === 0;
+        if (isWin) {
+            // show replace
+            const replaceButton = document.getElementsByClassName('game__button');
+            if (!replaceButton) return;
+            replaceButton.classList.add('show')
+                // show You WIN
+        }
+
+        selections = [];
+        return;
+    }
+
+    // in case not match
+    // remove active class for 2 li element
+    gameStatus = GAME_STATUS.BLOCKING;
+
+    setTimeout(() => {
+        selections[0].classList.remove('active');
+        selections[1].classList.remove('active');
+
+        // reset selections
+        selections = [];
+
+        gameStatus = GAME_STATUS.PLAYING;
+    }, 500);
+
+
+
 }
 
 function initColors() {
@@ -23,6 +66,8 @@ function initColors() {
     // bind to li > div.overlay
     const liList = getColorElementList();
     liList.forEach((liElement, index) => {
+        liElement.dataset.color = colorList[index];
+
         const overlayElement = liElement.querySelector('.overlay');
         if (overlayElement) overlayElement.style.backgroundColor = colorList[index];
     });
@@ -31,7 +76,10 @@ function initColors() {
 function attachEventForColorList() {
     const ulElement = getColorListElement();
     if (!ulElement) return;
+
+    // Event delegation 
     ulElement.addEventListener('click', (event) => {
+        if (event.target.tagName !== 'LI') return;
         handleColorClick(event.target);
     })
 }
